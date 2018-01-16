32847abcc8f6970609fb1cd10aed5d6feacc88e9
Murilo Avila <murilo.avila@duosystem.com>
2018-01-16 17:38:26 -0200

    Trazendo informacoes extras do Elenco

--- src/ErikaBundle/Service/ElencoService.php
+++ src/ErikaBundle/Service/ElencoService.php
@@ -26,13 +26,26 @@
 	    	$em = $this->entityManager;
 	    	$ator = $em->getRepository(Elenco::class)->findOneBy(array('idTmdbElc' => $actor->id));
 	    	if(empty($ator) || $ator == null){
-	    		
+	    		$ator_detalhes = $this->getDetails($actor->id);
 	    	}
 	    }
 
 	    public function newElencoCrew($crew)
 	    {
 	    	return $crew;
+	    }
+
+
+	    public function getDetails($people_id)
+	    {
+	    	$parameters = array(
+                "api_key"   => "c3d594f81aba4df6403f9f5441d639e0",
+            );
+
+			$mcurl = $this->container->get('erika.mcurl');
+
+			$mcurl->setBase_uri("https://api.themoviedb.org/3/");
+			return $mcurl->getJsonToObject('people/'.$people_id, $parameters);
 	    }
 	}
 ?>
